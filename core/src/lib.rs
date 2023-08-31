bc9d4aa9acf73bbc50f85ac4bdc5b47f368c7955
Matt <n01437978@unf.edu>
2023-08-31 16:58:10 -0400

    added into  for tree node and link

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,27 +1,55 @@
 #![allow(unused)]
+
+pub mod prisma;
+
 enum RelationshipType {
     Marriage(bool),
-    Affair
+    Affair,
 }
 
-//final output for graph shouldnt care about details only rendering stuff
-struct FamilyTreeNode {
-    id: String,
-    hidden: bool
+#[derive(Debug, Clone)]
+pub struct TreeLinkData {
+    id: i32,
 }
 
-struct TreeData {
-    nodes: Vec<FamilyTreeNode>,
+#[derive(Debug, Clone)]
+pub struct TreeLink {
+    source: TreeLinkData,
+    target: TreeLinkData,
+}
+
+impl Into<TreeNode> for prisma::person::Data {
+    fn into(self) -> TreeNode {
+        TreeNode { id: self.id, parent_id: self.parent_id, hidden: false}
+    }
+}
+
+impl Into<TreeLink> for prisma::relationship::Data {
+    fn into(self) -> TreeLink {
+        TreeLink { source: TreeLinkData { id: self.source_id }, target: TreeLinkData {id: self.target_id} }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TreeNode {
+    id: i32,
+    parent_id: Option<i32>,
+    hidden: bool,
+}
+
+#[derive(Debug)]
+pub struct TreeData {
+    nodes: Vec<TreeNode>,
+    links: Vec<TreeLink>,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+
     #[test]
     fn it_works() {
         //get list of people
-        //
-
     }
 }
--- core/src/main.rs
+++ core/src/main.rs
@@ -1,12 +1,24 @@
-#[allow(warnings, unused)]
-mod prisma;
- 
-use prisma::PrismaClient;
+use itertools::Itertools;
 use prisma_client_rust::NewClientError;
- 
+use wispcore::{prisma, TreeLink, TreeNode};
+
 #[tokio::main]
 async fn main() {
-    let client: Result<PrismaClient, NewClientError> = PrismaClient::_builder().build().await;
-    let result = client;
-    println!("{:#?}", result.unwrap().family().find_many(vec![]).exec().await.unwrap());
+    let client: Result<prisma::PrismaClient, NewClientError> =
+        prisma::PrismaClient::_builder().build().await;
+    let prisma = client.unwrap();
+
+    let m_s = prisma
+        .relationship()
+        .find_many(vec![])
+        .exec()
+        .await
+        .unwrap();
+    let m: Vec<TreeLink> = m_s.into_iter().map(|r| r.into()).collect_vec();
+
+    let people = prisma.person().find_many(vec![]).exec().await.unwrap();
+    let as_node: Vec<TreeNode> = people.into_iter().map(|p| p.into()).collect_vec();
+
+    println!("{:#?}", as_node);
+    println!("{:#?}", m);
 }
