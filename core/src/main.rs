1337b6f40b0e91555be2b851790d655132e81c5b
Matt <n01437978@unf.edu>
2023-08-31 15:26:42 -0400

    added pcr

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,4 +1,12 @@
-
-fn main() {
-
+#[allow(warnings, unused)]
+mod prisma;
+ 
+use prisma::PrismaClient;
+use prisma_client_rust::NewClientError;
+ 
+#[tokio::main]
+async fn main() {
+    let client: Result<PrismaClient, NewClientError> = PrismaClient::_builder().build().await;
+    let result = client;
+    println!("{:#?}", result.unwrap().family().find_many(vec![]).exec().await.unwrap());
 }
