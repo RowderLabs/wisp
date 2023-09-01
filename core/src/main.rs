ff1140502b27b7f102eb94e4d0171a733d105d18
Matt <n01437978@unf.edu>
2023-08-31 20:10:46 -0400

    added parent child relationship

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,6 +1,5 @@
-use itertools::Itertools;
 use prisma_client_rust::NewClientError;
-use wispcore::{prisma, TreeLink, TreeNode};
+use wispcore::prisma::{self, person};
 
 #[tokio::main]
 async fn main() {
@@ -8,17 +7,14 @@ async fn main() {
         prisma::PrismaClient::_builder().build().await;
     let prisma = client.unwrap();
 
-    let m_s = prisma
-        .relationship()
-        .find_many(vec![])
+    let lord = prisma
+        .person()
+        .find_first(vec![person::id::equals(2)])
+        .with(person::children::fetch(vec![]))
         .exec()
         .await
+        .unwrap()
         .unwrap();
-    let m: Vec<TreeLink> = m_s.into_iter().map(|r| r.into()).collect_vec();
 
-    let people = prisma.person().find_many(vec![]).exec().await.unwrap();
-    let as_node: Vec<TreeNode> = people.into_iter().map(|p| p.into()).collect_vec();
-
-    println!("{:#?}", as_node);
-    println!("{:#?}", m);
+    println!("{:#?}", lord.children);
 }
