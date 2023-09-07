ecce17cda922ab875707efc032ee676a8065fa39
Matt <n01437978@unf.edu>
2023-09-07 17:17:49 -0400

    added rspc to frontend

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,15 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use snafu::ResultExt;
+use wispcore::prisma::PrismaClient;
+use std::sync::Arc;
+use wispapp::api;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), snafu::Whatever> {
+    let router = api::new().build().arced();
+    let prisma: Arc<PrismaClient> = wispcore::prisma::PrismaClient::_builder()
+        .build()
+        .await
+        .whatever_context("Failed to initialize prisma client")?
+        .into();
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(rspc::integrations::tauri::plugin(router, move || {
+            api::Ctx {
+                client: prisma.clone(),
+            }
+        }))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    Ok(())
 }
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { rspc, client, queryClient } from "./rspc/router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <rspc.Provider client={client} queryClient={queryClient}>
+      <App />
+    </rspc.Provider>
   </React.StrictMode>,
 );
