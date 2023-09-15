8a231312f6437e61145a71c1d39798145836baae
Matt <n01437978@unf.edu>
2023-09-15 13:14:13 -0400

    removed binder frontend code

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,10 +4,6 @@ import { rspc } from "./rspc/router";
 import * as d3 from "d3";
 import pfp from "./assets/pfp.png";
 import { createChildPath } from "./paths";
-import { HiOutlineMap, HiUser, HiUsers } from "react-icons/hi";
-import Binder from "./components/Binder";
-import Node from "./components/Node";
-import useBinderData from "./hooks/useBinder";
 
 function App() {
   const treeContainerRef = useRef<SVGSVGElement>(null);
@@ -75,15 +71,9 @@ function App() {
         })
     : [];
 
-  const { characters } = useBinderData();
-
   return (
     <div className="flex gap-4 h-screen">
-      <div className="w-[300px] h-full shadow-md border">
-        <Binder data={[{ id: "CHARACTERS", name: "Characters", children: characters, icon: <HiUsers /> }]}>
-          {Node}
-        </Binder>
-      </div>
+      <div className="w-[300px] h-full shadow-md border"></div>
       <div style={{ width: "1200px", height: "600px" }}>
         <svg ref={treeContainerRef} viewBox="0 0 1200 600">
           <g ref={treeRef}>
