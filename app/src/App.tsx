bd01f3fe7e53daea6f95d5494c65f7d66050fa20
Madelyn Raymond <n01440101@unf.edu>
2023-09-21 21:27:09 -0400

    added character sheet banner & photo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,6 +7,7 @@ import { createChildPath } from "./paths";
 import Binder from "./ui/Binder";
 import FamilyTree from "./ui/FamilyTree";
 import { AttributePanel } from "./AttributePanel";
+import { CharacterSheetBanner } from "./CharacterSheetBanner";
 
 function App() {
 
@@ -15,8 +16,14 @@ function App() {
       <div className="h-full w-[300px] shadow-md border">
         <Binder />
       </div>
-      <div>
-        <AttributePanel />
+      <div className="basis-full mx-20">
+        <div className="relative">
+          <CharacterSheetBanner />
+          <div className="absolute h-48 w-48 bg-blue-500 bottom-[50%] transform translate-y-[105%] left-10 rounded-md border-8 border-white"></div>
+        </div>
+        <div className="mt-32">
+          <AttributePanel />
+        </div>
       </div>
     </div>
   );
---
+++ app/src/CharacterSheetBanner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CharacterSheetBanner = () => {
+  return (
+    <div className="bg-blue-200 min-h-[200px] flex flex-col gap-4 rounded-md border p-6">
+        
+    </div>
+  )
+}
