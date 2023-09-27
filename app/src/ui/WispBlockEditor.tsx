8031a5ef2d8b0bee5d69976f3f4e055a08f97b56
mreid21 <n01437978@unf.edu>
2023-09-27 10:16:55 -0400

    removed side menu from editor

--- app/src/ui/WispBlockEditor.tsx
+++ app/src/ui/WispBlockEditor.tsx
@@ -1,8 +1,20 @@
 import { BlockNoteEditor } from "@blocknote/core";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import {
+  BlockNoteView,
+  FormattingToolbarPositioner,
+  HyperlinkToolbarPositioner,
+  SlashMenuPositioner,
+  useBlockNote,
+} from "@blocknote/react";
 import "@blocknote/core/style.css";
 
 export default function WispBlockEditor() {
   const editor: BlockNoteEditor = useBlockNote({});
-  return <BlockNoteView editor={editor} theme={"light"} />;
+  return (
+    <BlockNoteView editor={editor} theme={"light"}>
+      <FormattingToolbarPositioner editor={editor} />
+      <HyperlinkToolbarPositioner editor={editor} />
+      <SlashMenuPositioner editor={editor} />
+    </BlockNoteView>
+  );
 }
