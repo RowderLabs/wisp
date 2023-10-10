6efb2543617bffd18bf4c848bc9a120d6471438e
mreid21 <n01437978@unf.edu>
2023-10-10 13:36:41 -0400

    added scuffed tree context

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,24 +5,35 @@ import { EditableInline } from "./ui/EditableInline";
 import { useEditCharacter } from "./hooks/useEditCharacter";
 import WispEditor from "./ui/WispEditor";
 import UploadableImage from "./ui/UploadableImage";
+import { FileTreeContextProps, createFileTree } from "./context/FileTreeContext";
+
+const Tree = ({useFileTree}: {useFileTree: () => FileTreeContextProps<unknown>}) => {
+  const {nodes} = useFileTree()
+  return <div>{JSON.stringify(nodes)}</div>;
+};
 
 function App() {
   const { data: character } = rspc.useQuery(["characters.with_id", 1]);
   const { changeName } = useEditCharacter();
+  const { data: characters } = rspc.useQuery(["binder.characters", null]);
+  const {FileTreeContext, useFileTree} = createFileTree();
 
   return (
     <div className="flex gap-4 h-screen">
       <div className="h-full w-[300px] shadow-md border">
+        <FileTreeContext.Provider value={{ nodes: characters }}>
+          <Tree useFileTree={useFileTree}/>
+        </FileTreeContext.Provider>
         <Binder />
       </div>
       <div className="basis-full mx-20">
         <div className="relative">
           <Banner className="relative">
-            <UploadableImage uploadedImage={{position: 'right', fit: 'contain'}}/>
+            <UploadableImage uploadedImage={{ position: "right", fit: "contain" }} />
           </Banner>
           <div className="flex gap-4 px-4 py-2 items-start">
             <div className="relative h-48 w-48 bg-white rounded-md mt-[-100px] border shadow-sm">
-              <UploadableImage uploadedImage={{position: 'right', fit: 'contain'}}/>
+              <UploadableImage uploadedImage={{ position: "right", fit: "contain" }} />
             </div>
             {character && (
               <EditableInline
---
+++ app/src/context/FileTreeContext.tsx
@@ -0,0 +1,19 @@
+import React, { PropsWithChildren, useState } from "react";
+
+export type FileTreeContextProps<T = any> = {
+  nodes?: { [key: string]: T[] };
+};
+
+
+export function createFileTreeContext<T = unknown>() {
+  return React.createContext<FileTreeContextProps<T> | null>(null)
+}
+
+export function createFileTree<T = unknown>() {
+  const FileTreeContext = createFileTreeContext<T>()
+  if (!FileTreeContext) throw new Error('oh no')
+  const useFileTree = () => React.useContext(FileTreeContext) as FileTreeContextProps<T>
+
+  return {FileTreeContext, useFileTree}
+}
+
