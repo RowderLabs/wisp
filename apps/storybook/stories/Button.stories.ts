f7e827e5cb01d3e797673d5c5de4bce3dcf9166a
Matt <n01437978@unf.edu>
2023-10-20 14:59:21 -0400

    added storybook to monorepo

---
+++ apps/storybook/stories/Button.stories.ts
@@ -0,0 +1,12 @@
+// Button.stories.ts
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from '@wisp/ui';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Basic: Story = {};
--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,3 +1,3 @@
 export const Button = () => {
-    return <button className="border-none px-2 py-1 bg-blue-500 text-white">Hello world</button>
+    return <button className="border-none rounded-md px-2 py-1 bg-red-500 text-white">Hello world</button>
 }
