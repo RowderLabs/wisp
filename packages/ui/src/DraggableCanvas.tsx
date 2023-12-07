09f09dd6b5945630258a3af70219e0769cb8fe94
mreid21 <reidmt.21@gmail.com>
2023-12-07 14:40:44 -0500

    added restrict to window edges with working deps

--- packages/ui/src/DraggableCanvas.tsx
+++ packages/ui/src/DraggableCanvas.tsx
@@ -1,4 +1,5 @@
 import { DndContext, MouseSensor, UniqueIdentifier, useDraggable, useSensor } from "@dnd-kit/core";
+import { restrictToParentElement } from "@dnd-kit/modifiers";
 import { CSS } from "@dnd-kit/utilities";
 import React, { PropsWithChildren, forwardRef, useImperativeHandle } from "react";
 import { useState } from "react";
@@ -36,6 +37,7 @@ export const DraggableCanvas = forwardRef<DraggableCanvasHandle, DraggableCanvas
     return (
       <DndContext
         sensors={[mouseSensor]}
+        modifiers={[restrictToParentElement]}
         onDragEnd={({ active, delta }) => {
           const activeItem = canvasItems.find((item) => item.id === active.id);
           if (!activeItem) return;
