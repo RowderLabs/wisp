69b68fb698cc8fb8194b319260884573a4c054a1
Madelyn <n01440101@unf.edu>
2023-12-16 19:32:50 -0500

    updated prisma model

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,25 +1,5 @@
 #![allow(unused)]
 use std::collections::HashSet;
-
 use itertools::Itertools;
-use prisma::{person, relationship};
 pub mod prisma;
-pub mod tree;
-pub mod seed;
 pub mod api;
-
-enum RelationshipType {
-    Marriage(bool),
-    Affair,
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        //get list of people
-    }
-}
