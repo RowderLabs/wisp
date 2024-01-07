f5c695300c7e39afc0aed2412d1aa263e64703f0
Matt <reidmt.21@gmail.com>
2024-01-07 15:58:57 -0500

    added reducer

--- packages/ui/src/Transform.tsx
+++ packages/ui/src/Transform.tsx
@@ -1,28 +1,21 @@
 import { DndContext, DragMoveEvent, useDraggable } from "@dnd-kit/core";
-import { useMeasure } from "@uidotdev/usehooks";
 import { nanoid } from "nanoid";
-import React, {
-  PropsWithChildren,
-  RefCallback,
-  SetStateAction,
-  createContext,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import React, { Dispatch, PropsWithChildren, createContext, useContext, useEffect, useReducer, useRef } from "react";
+
+type Maybe<T extends unknown> = T extends object
+  ? {
+      [K in keyof T]: T[K] | undefined;
+    }
+  : T | undefined;
 
 type TransformContextType = {
   transformRef: React.MutableRefObject<HTMLDivElement | null>;
-  width: number | null;
-  height: number | null;
-  onTranformEnd: TransformEndEvent;
-  x: number | null;
-  y: number | null;
-  setWidth: React.Dispatch<SetStateAction<number | null>>;
-  setHeight: React.Dispatch<SetStateAction<number | null>>;
-  setX: React.Dispatch<SetStateAction<number | null>>;
-  setY: React.Dispatch<SetStateAction<number | null>>;
+  width: Maybe<number>;
+  height: Maybe<number>;
+  onTranformEnd?: TransformEndEvent;
+  x: Maybe<number>;
+  y: Maybe<number>;
+  dispatch: Dispatch<TransformAction>;
 };
 
 const TransformContext = createContext<TransformContextType | null>(null);
@@ -33,47 +26,82 @@ export const useTransformContext = () => {
   return context as TransformContextType;
 };
 
-type TransformEndEvent = (
-  event: Partial<{ x: number; y: number; width: number | null; height: number | null }>
-) => void;
-
-type TransformProps = {
-  initialWidth: number | null,
-  initialHeight: number | null;
-  onTranformEnd: TransformEndEvent;
+export const useTransform = (): Pick<TransformContextType, "x" | "y" | "width" | "height" | "transformRef"> => {
+  return useTransformContext();
 };
 
-function Root({ children, onTranformEnd, initialHeight = null, initialWidth = null }: PropsWithChildren<TransformProps>) {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [width, setWidth] = useState<number | null>(initialWidth);
-  const [height, setHeight] = useState<number | null>(initialHeight);
-  const [x, setX] = useState<number | null>(null);
-  const [y, setY] = useState<number | null>(null);
+type TransformEndEvent = (event: Partial<Maybe<Transform>>) => void;
+
+type TransformProps = {
+  initial: Partial<Maybe<Transform>>;
+  onTranformEnd?: TransformEndEvent;
+};
+
+type Transform = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+type TransformState = Maybe<Transform> & {};
+type TransformAction =
+  | { type: "SET_X"; payload: number }
+  | { type: "SET_Y"; payload: number }
+  | { type: "SET_WIDTH"; payload: number }
+  | { type: "SET_HEIGHT"; payload: number };
+
+function transformReducer(state: TransformState, action: TransformAction): TransformState {
+  switch (action.type) {
+    case "SET_WIDTH": {
+      return { ...state, width: action.payload };
+    }
+    case "SET_HEIGHT": {
+      return { ...state, height: action.payload };
+    }
+    case "SET_X": {
+      return { ...state, x: action.payload };
+    }
+    case "SET_Y": {
+      return { ...state, y: action.payload };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+function Root({ children, onTranformEnd, initial }: PropsWithChildren<TransformProps>) {
+  const transformRef = useRef<HTMLDivElement | null>(null);
+  const [transformState, dispatch] = useReducer(transformReducer, {
+    width: initial.width,
+    height: initial.height,
+    x: initial.x,
+    y: initial.y,
+  });
 
   useEffect(() => {
-    if (ref.current) {
-      setWidth(ref.current.getBoundingClientRect().width);
-      setHeight(ref.current.getBoundingClientRect().height);
+    if (transformRef.current) {
+      dispatch({ type: "SET_WIDTH", payload: transformRef.current.getBoundingClientRect().width });
+      dispatch({ type: "SET_HEIGHT", payload: transformRef.current.getBoundingClientRect().height });
     }
-  }, [ref]);
+  }, [transformRef]);
 
   return (
-    <TransformContext.Provider
-      value={{ onTranformEnd, setHeight, setWidth, transformRef: ref, width, height, x, y, setX, setY }}
-    >
+    <TransformContext.Provider value={{ dispatch, onTranformEnd, transformRef, ...transformState }}>
       {children}
     </TransformContext.Provider>
   );
 }
 
 function Resize({ children }: PropsWithChildren) {
-  const { width, height, setWidth, setHeight, onTranformEnd } = useTransformContext();
-  const dragStartWidth = useRef<number | null>(null);
-  const dragStartHeight = useRef<number | null>(null);
+  const { width, height, dispatch, onTranformEnd } = useTransformContext();
+  const dragStartWidth = useRef<Maybe<number>>();
+  const dragStartHeight = useRef<Maybe<number>>();
 
   const resizeBottomRight = ({ delta }: DragMoveEvent) => {
-    setWidth(dragStartWidth.current! + delta.x);
-    setHeight(dragStartHeight.current! + delta.y);
+    dispatch({ type: "SET_WIDTH", payload: dragStartWidth.current! + delta.x });
+    dispatch({ type: "SET_HEIGHT", payload: dragStartHeight.current! + delta.y });
   };
 
   const setStartingDimensions = () => {
@@ -85,7 +113,9 @@ function Resize({ children }: PropsWithChildren) {
     <DndContext
       onDragStart={setStartingDimensions}
       onDragMove={resizeBottomRight}
-      onDragEnd={() => onTranformEnd({ width, height })}
+      onDragEnd={() => {
+        if (onTranformEnd) onTranformEnd({ width, height });
+      }}
     >
       {children}
     </DndContext>
