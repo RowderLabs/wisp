95c38df76c120b552e973f490678054fefd2518b
Matt <reidmt.21@gmail.com>
2024-01-07 15:57:55 -0500

    added reducer

--- apps/app/src/routes/workspace.characters.$characterId.tsx
+++ apps/app/src/routes/workspace.characters.$characterId.tsx
@@ -1,9 +1,9 @@
 import { FileRoute } from "@tanstack/react-router";
-import { PanelCanvas } from "@wisp/ui";
+import { Transform, useTransform } from "@wisp/ui";
 import { CharacterSummary } from "../ui/CharacterSummary";
 
 export const Route = new FileRoute("/workspace/characters/$characterId").createRoute({
-  loader: ({context, params}) =>
+  loader: ({ context, params }) =>
     context.rspc.queryClient.ensureQueryData({
       queryKey: ["characters.with_id", { id: params.characterId }],
       queryFn: () => context.rspc.client.query(["characters.with_id", params.characterId]),
@@ -16,9 +16,22 @@ function WorkspaceCharacterSheetPage() {
   return (
     <div className="flex w-full px-4">
       <div className="basis-full" style={{ height: "800px" }}>
-        <PanelCanvas />
+        <Transform.Root initial={{ width: 150, height: 300 }}>
+          <Transform.Resize>
+            <Box />
+          </Transform.Resize>
+        </Transform.Root>
       </div>
       {character && <CharacterSummary name={character?.fullName} />}
+    </div>
+  );
+}
+
+function Box() {
+  const transformStyles = useTransform();
+  return (
+    <div style={{ ...transformStyles }} className="bg-blue-200 border-blue-400 relative">
+      <Transform.ResizeHandle />
     </div>
   );
 }
