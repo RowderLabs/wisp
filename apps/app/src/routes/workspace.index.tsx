491b357dc7c5b1f8563427c3288ddaa39e396b73
Matt <reidmt.21@gmail.com>
2024-02-17 18:01:48 -0500

    added facts component

--- apps/app/src/routes/workspace.index.tsx
+++ apps/app/src/routes/workspace.index.tsx
@@ -8,5 +8,26 @@ export const Route = createFileRoute("/workspace/")({
 });
 
 function WorkspaceHome() {
-  return <p>Hello From Workspace</p>;
+  return <div>
+    <FactList/>
+  </div>;
+}
+
+function Fact() {
+  return (
+    <div className="p-2 flex justify-between items-center border-b">
+      <p>First Name</p>
+      <p>Matt</p>
+    </div>
+  );
+}
+
+export function FactList() {
+  return (
+    <div className="bg-white rounded-md border text-sm p-8 mx-auto flex flex-col gap-2 max-w-[600px]">
+      <Fact />
+      <Fact />
+      <Fact />
+    </div>
+  );
 }
