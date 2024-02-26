6718813d80dfeecfe78394c73084ed0cf5c8b257
Matt <reidmt.21@gmail.com>
2024-02-26 17:40:17 -0500

    added debounced auto form submit

--- packages/ui/src/Form.tsx
+++ packages/ui/src/Form.tsx
@@ -5,12 +5,12 @@ import { useFormContext } from "react-hook-form";
 
 interface Props<T extends FieldValues> extends Omit<ComponentProps<"form">, "onSubmit"> {
   form: UseFormReturn<T>;
-  onSubmit: SubmitHandler<T>;
+  onSubmit?: SubmitHandler<T>;
 }
 
 export const Form = <T extends FieldValues>({ form, onSubmit, children, ...props }: Props<T>) => (
   <FormProvider {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} {...props}>
+    <form onSubmit={onSubmit && form.handleSubmit(onSubmit)} {...props}>
       <fieldset>{children}</fieldset>
     </form>
   </FormProvider>
