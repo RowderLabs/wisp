d76e93e03c805ff03e9de5b94026b714ccce08d5
Matt <reidmt.21@gmail.com>
2024-02-29 18:44:41 -0500

    disabled spellcheck

--- packages/ui/src/TextEditor.tsx
+++ packages/ui/src/TextEditor.tsx
@@ -97,7 +97,7 @@ export default function TextEditor({ className, features, editorTheme, onChange,
         )}
 
         <RichTextPlugin
-          contentEditable={<ContentEditable className="editor editor-input cursor-text" />}
+          contentEditable={<ContentEditable spellCheck={false} className="editor editor-input cursor-text" />}
           placeholder={null}
           ErrorBoundary={LexicalErrorBoundary}
         />
