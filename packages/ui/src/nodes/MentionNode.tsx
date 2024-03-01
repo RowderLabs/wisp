3a3ac948ea494a14d7c3636575e52536d4c101f4
Matt <reidmt.21@gmail.com>
2024-02-29 19:02:41 -0500

    fixed mentions

--- packages/ui/src/nodes/MentionNode.tsx
+++ packages/ui/src/nodes/MentionNode.tsx
@@ -2,13 +2,15 @@ import { DecoratorNode, NodeKey, SerializedLexicalNode, Spread, createCommand } 
 import { ReactNode } from "react";
 import { cva } from "class-variance-authority";
 import { Link } from "@tanstack/react-router";
+import { EntityType } from "@wisp/client/src/bindings";
 
-export const INSERT_MENTION_NODE_COMMAND = createCommand<{ id: string; name: string }>();
+export const INSERT_MENTION_NODE_COMMAND = createCommand<{ id: string; name: string, entityType: EntityType }>();
 
 export type SerializedMentionNode = Spread<
   {
     id: string;
     name: string;
+    entityType: EntityType
   },
   SerializedLexicalNode
 >;
@@ -16,19 +18,21 @@ export type SerializedMentionNode = Spread<
 export class MentionNode extends DecoratorNode<ReactNode> {
   __id: string;
   __name: string;
+  __entityType: EntityType
 
   static getType(): string {
     return "mention";
   }
 
   static clone(node: MentionNode): MentionNode {
-    return new MentionNode(node.__id, node.__name, node.__key);
+    return new MentionNode(node.__id, node.__name, node.__entityType, node.__key);
   }
 
-  constructor(id: string, name: string, key?: NodeKey) {
+  constructor(id: string, name: string, entityType: EntityType, key?: NodeKey) {
     super(key);
     this.__id = id;
     this.__name = name;
+    this.__entityType = entityType
   }
 
   createDOM(): HTMLElement {
@@ -39,40 +43,48 @@ export class MentionNode extends DecoratorNode<ReactNode> {
     return false;
   }
 
-
-
   exportJSON(): SerializedMentionNode {
     return {
       id: this.__id,
       name: this.__name,
+      entityType: this.__entityType,
       type: this.getType(),
       version: 1,
     };
   }
 
   static importJSON(serializedNode: SerializedMentionNode): MentionNode {
-    return $createMentionNode(serializedNode.id, serializedNode.name);
+    return $createMentionNode(serializedNode.id, serializedNode.name, serializedNode.entityType);
   }
 
   decorate(): ReactNode {
-    return <Mention id={this.__id} name={this.__name} />;
+    return <Mention id={this.__id} name={this.__name} entityType={this.__entityType} />;
   }
 }
 
 type MentionProps = {
   id: string;
   name: string;
+  entityType: EntityType
 };
 
-export function $createMentionNode(id: string, name: string) {
-  return new MentionNode(id, name);
+export function $createMentionNode(id: string, name: string, entityType: EntityType) {
+  return new MentionNode(id, name, entityType);
 }
 
-const mentionVariants = cva(["text-sm rounded-md bg-blue-200 p-0.5 text-blue-600/80", "hover:text-white hover:bg-blue-400/80"]);
+const mentionVariants = cva([
+  "text-sm rounded-md bg-blue-200 p-0.5 text-blue-600/80",
+  "hover:text-white hover:bg-blue-400/80",
+]);
 
-function Mention({ name, id }: MentionProps) {
+function Mention({ name, id, entityType }: MentionProps) {
   return (
-    <Link className={mentionVariants()} to="/workspace/characters/$characterId" params={{ characterId: id }}>
+    <Link
+      className={mentionVariants()}
+      to="/workspace/entity/$entityId"
+      search={{ type: entityType }}
+      params={{ entityId: id }}
+    >
       @{name}
     </Link>
   );
